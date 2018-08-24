e3ad5ef4fa482971800265254990d71b37785464
Marc <marc.maninang@gmail.com>
2018-08-23 22:29:37 -0700

    Added squares with red hover

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function createDivs {
-
+for (var i = 0; i < 17; i++) {
+    const makeDiv = document.createElement('div');
+    makeDiv.classList.add('square');
+    document.getElementById('container').appendChild(makeDiv);
 }
-
-const main = {}
