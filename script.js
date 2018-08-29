845601337216dc31159f20b36f8763c692cb6c49
Marc <marc.maninang@gmail.com>
2018-08-28 23:03:02 -0700

    create grid function

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-for (var i = 0; i < 256; i++) {
-    const makeDiv = document.createElement('div');
-    makeDiv.classList.add('square');
-    document.getElementById('container').appendChild(makeDiv);
+// Dynamically create grid
+function createGrid() {
+    for (var i = 0; i < 256; i++) {
+        const makeDiv = document.createElement('div');
+        makeDiv.classList.add('square');
+        document.getElementById('container').appendChild(makeDiv);
+    }
 }
+createGrid();
+const squares = document.getElementById("square");
