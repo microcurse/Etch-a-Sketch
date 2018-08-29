f37ff728a72e76a18982abb67ffcbac4956c4c78
Marc Maninang <Marc.Maninang@WINSFORD.COM>
2018-08-29 15:31:17 -0700

    Adding event listener for all elements with square

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ function createGrid() {
     for (var i = 0; i < 256; i++) {
         const makeDiv = document.createElement('div');
         makeDiv.classList.add('square');
+        makeDiv.setAttribute("id", "square");
         document.getElementById('container').appendChild(makeDiv);
     }
 }
 createGrid();
-const squares = document.getElementById("square");
+const squares = document.getElementsByClassName("square");
+squares.forEach((square) => {
+    squares.addEventListener("mouseenter", function(e) {
+        e.target.style.color = "blue";
+    });
+});
