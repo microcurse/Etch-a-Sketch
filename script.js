cff5274738195a15e79054432abf9700ded8b229
Marc <marc.maninang@gmail.com>
2018-09-02 08:37:10 -0700

    cleaned up html containers and css

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function createGrid() {
         const makeDiv = document.createElement('div');
         makeDiv.classList.add('square');
         // makeDiv.setAttribute("id", "square");
-        document.getElementById('container').appendChild(makeDiv);
+        document.getElementById('grid-container').appendChild(makeDiv);
     }
 }
 createGrid();
@@ -14,5 +14,12 @@ Array.from(document.getElementsByClassName("square")).forEach(
     function(e) {
         e.addEventListener("mouseenter", function() {
             e.classList.add("darken");
+    });
+});
+// Find all squares and remove red color when unhovered
+Array.from(document.getElementsByClassName("square")).forEach(
+    function(e) {
+        e.addEventListener("mouseleave", function() {
+            e.classList.remove("darken");
     });
 });
